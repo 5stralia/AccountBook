f453804746f1bf4718264d09dd2c55b6db583e1f
hoju <5stralia@gmail.com>
2021-08-29 03:56:58 +0900

    구글 로그인

--- AccountBook/ProfileViewController/ProfileViewController.swift
+++ AccountBook/ProfileViewController/ProfileViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+import Firebase
+import GoogleSignIn
 
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var signOutButton: UIButton!
+    
+    private var authHandle: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.authHandle = Auth.auth().addStateDidChangeListener { auth, user in
+            // 로그인 상태 변경 리스너
+            if let name = user?.displayName {
+                self.signOutButton.setTitle("\(name) sign out", for: .normal)
+            }
+        }
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if Auth.auth().currentUser == nil {
+            let signInViewController = SignInViewController()
+            signInViewController.modalPresentationStyle = .fullScreen
+            self.present(signInViewController, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let handle = self.authHandle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
